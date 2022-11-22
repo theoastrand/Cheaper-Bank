97172c26322065175b97afb8a8b0f277e1efb7db
Theodor Åstrand <theo@Olivers-MacBook-Pro-2.local>
2022-11-22 19:34:55 +0100

    je

--- Cheaper_Bank.java
+++ Cheaper_Bank.java
@@ -3,6 +3,7 @@ package CheaperBanking;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Cheaper_Bank {
     private static BufferedReader reader;
@@ -34,11 +35,32 @@ public class Cheaper_Bank {
             String[] names = name.split("_");
             if (names.length < 2 || names.length > 3) validAccount = false;
             if (String.join("", names).length() > 20) validAccount = false;
-            
+
             String age = instruction[2];
+            if (!isNumeric(age)) validAccount = false;
+
             String work = instruction[3];
+
+            if (validAccount) {
+                BankAccount newAccount = new BankAccount(name, Integer.parseInt(age), work);
+            } else {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Data fro new account was incorrect, pleasr enter new: ");
+                String newLine = sc.nextLine();
+                sc.close();
+                parseLine(newLine); //Risky som fan
+            }
         } else if (command.equals("-l")) {
 
         }
     }
+
+    private static boolean isNumeric(String str) { 
+        try {  
+          Double.parseDouble(str);  
+          return true;
+        } catch(NumberFormatException e){  
+          return false;  
+        }  
+      }
 }
